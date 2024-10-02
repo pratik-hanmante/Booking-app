7bf4eafee1c3a40eba84ccb7fa2502a3c7a43301
pratik hanmante <pratikhanmante3@gmail.com>
2024-10-02 16:22:46 +0530

    first commit

---
+++ src/main/java/com/pratikdev/PratikHotel/PratikHotelApplication.java
@@ -0,0 +1,13 @@
+package com.pratikdev.PratikHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratikHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PratikHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratikdev/PratikHotel/PratikHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratikdev.PratikHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratikHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
